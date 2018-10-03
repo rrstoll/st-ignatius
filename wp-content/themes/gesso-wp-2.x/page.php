293595ad87664ccd3082f20ca09f1b8488473521
Russell Stoll <rrstoll@forumone.com>
2018-10-03 13:01:25 -0400

    Fixes to meta query logic

--- wp-content/themes/gesso-wp-2.x/page.php
+++ wp-content/themes/gesso-wp-2.x/page.php
@@ -28,31 +28,41 @@ $categories = $post->terms( 'category' );
 // Get only the first category from the array
 $context['category'] = reset( $categories );
 // Define generic templates.
-$templates = array( 
-	'page-' . $post->post_name . '.twig', 
-	'page-' . $post->ID . '.twig', 
-	'page.twig' 
+$templates = array(
+	'page-' . $post->post_name . '.twig',
+	'page-' . $post->ID . '.twig',
+	'page.twig'
 );
 // Set the Homepage template.
 if ( is_front_page()){
 	// get latest three posts
     $args = array(
-		'numberpost' => -1,
 		'posts_per_page' => 6,
 		'post_type' => 'post',
-		'meta_key' => 'featured_item',
-		'meta_value' => '1',
-    );
+		'meta_query' => array(
+			‘relation’ => ‘OR’,
+			array(
+				'key' => 'featured_item',
+				'value' => '1',
+				'compare' => 'LIKE'
+			)
+		)
+	);
 	$context['posts'] = Timber::get_posts($args);
 
-	// get latest three posts
+	// get non-feature posts
     $args2 = array(
-		'numberpost' => -1,
 		'posts_per_page' => 10,
 		'post_type' => 'post',
-		'meta_key' => 'featured_item',
-    	'meta_value' => '',
-    );
+		'meta_query' => array(
+			‘relation’ => ‘OR’,
+			array(
+				'key' => 'featured_item',
+				'value' => '1',
+				'compare' => 'NOT LIKE'
+			)
+		)
+	);
 	$context['news'] = Timber::get_posts($args2);
 
 	array_unshift( $templates, 'front-page.twig' );
